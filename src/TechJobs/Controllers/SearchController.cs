99dcd1184be39eff3bca8c8acbb395a0d8feab40
Gage <gw_eveland@hotmail.com>
2018-10-03 21:55:03 -0500

    Finished?

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -6,11 +6,31 @@ namespace TechJobs.Controllers
 {
     public class SearchController : Controller
     {
+        private static List<Dictionary<string, string>> jobResults = new List<Dictionary<string, string>>();
+
         public IActionResult Index()
         {
+            if (jobResults.Count > 0)
+            {
+                ViewBag.jobs = jobResults;
+            }
             ViewBag.columns = ListController.columnChoices;
             ViewBag.title = "Search";
             return View();
+            
+        }
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            jobResults.Clear();
+            jobResults = JobData.FindByColumnAndValue(searchType, searchTerm);
+            if (jobResults.Count == 0)
+            {
+                Dictionary<string, string> resultToAdd = new Dictionary<string, string>();
+                resultToAdd.Add("noResult", searchTerm);
+                jobResults.Add(resultToAdd);
+
+            }
+            return Redirect("Index");
         }
 
         // TODO #1 - Create a Results action method to process 
